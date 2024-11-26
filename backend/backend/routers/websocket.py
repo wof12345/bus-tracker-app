7abbd015eca40f0583a75d08dd1067b5bff60c3f
wof12345 <lsampercot@gmail.com>
2024-11-26 19:40:06 +0600

    feat : add mobile gps functionality

--- backend/backend/routers/websocket.py
+++ backend/backend/routers/websocket.py
@@ -9,7 +9,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from asyncio import run
 from backend.services.vehicles import update_vehicle, get_vehicle
 from backend.services.routes import get_route
-
+from datetime import datetime
 
 router = APIRouter()
 
@@ -24,31 +24,46 @@ simulationDetails = {}
 
 def convert_object_ids_to_str(data):
     """
-    Recursively converts all ObjectId instances in a nested dictionary or list to strings.
+    Recursively converts all ObjectId and datetime instances in a nested dictionary or list to strings.
 
     Args:
         data (dict | list): The dictionary or list to process.
 
     Returns:
-        dict | list: The processed dictionary or list with ObjectIds converted to strings.
+        dict | list: The processed dictionary or list with ObjectIds and datetimes converted to strings.
     """
     if isinstance(data, dict):
         return {
             key: convert_object_ids_to_str(value)
             if isinstance(value, (dict, list))
             else str(value)
-            if isinstance(value, ObjectId)
+            if isinstance(
+                value, (ObjectId, datetime)
+            )  # Convert ObjectId and datetime to string
             else value
             for key, value in data.items()
         }
     elif isinstance(data, list):
         return [convert_object_ids_to_str(item) for item in data]
     else:
-        return data
+        return str(data) if isinstance(data, (ObjectId, datetime)) else data
+
+
+def get_and_update_vehicle(data):
+    license = data['license']
+    lat = data['lat']
+    lng = data['lng']
+
+    vehicle = get_vehicle({'license': license})
+
+    if not vehicle:
+        return {'detail': 'Vehicle not found'}
+
+    current_coordinates = [lat, lng]
+    update_vehicle({'current_coordinates': current_coordinates}, vehicle['_id'])
 
 
 def simulateBus(id):
-    vehicle_col = database['vehicles']
     vehicle = get_vehicle({'_id': ObjectId(id)})
     returnObject = {}
 
@@ -95,9 +110,70 @@ def simulateBus(id):
     return vehicle
 
 
+def getBusData(id):
+    vehicle = get_vehicle({'_id': ObjectId(id)})
+    returnObject = {}
+
+    if not vehicle:
+        return {'detail': 'Vehicle not found'}
+
+    if not vehicle['route']:
+        return {'detail': 'Vehicle has no route'}
+
+    route = get_route({'_id': ObjectId(vehicle['route']['_id'])})
+
+    if not route:
+        return {'detail': 'Route record not found'}
+
+    vehicle_id = str(vehicle['_id'])
+
+    returnObject = vehicle
+
+    vehicle_last_move = None
+    last_coordinates = None
+    current_coordinates = vehicle['current_coordinates']
+
+    if 'last_coordinates' in vehicle:
+        last_coordinates = vehicle['last_coordinates']
+
+    returnObject['coordinates'] = current_coordinates
+
+    current_date = datetime.now()
+    if 'last_move' in vehicle:
+        vehicle_last_move = vehicle['last_move']
+
+    status = 'Stopped'
+
+    if vehicle_last_move:
+        difference = current_date - vehicle_last_move
+
+        print(difference, 'diff')
+
+        if (
+            abs(difference.total_seconds()) < 30
+            and last_coordinates != current_coordinates
+        ):
+            status = 'Running'
+
+    returnObject['coordinates'] = current_coordinates
+    update_vehicle(
+        {
+            'status': status,
+            'last_move': datetime.now(),
+            'last_coordinates': current_coordinates,
+        },
+        vehicle_id,
+    )
+
+    vehicle = convert_object_ids_to_str(returnObject)
+
+    return vehicle
+
+
 async def broadCastLiveData(vehicle_id):
     try:
-        vehicle = simulateBus(vehicle_id)
+        # vehicle = simulateBus(vehicle_id)
+        vehicle = getBusData(vehicle_id)
 
         print(active_client_sessions)
 
@@ -122,8 +198,13 @@ async def websocket_endpoint(websocket: WebSocket):
         while True:
             data = await websocket.receive_text()
 
-            vehicle = json.loads(data)
-            vehicle_id = vehicle['_id']
+            data = json.loads(data)
+
+            if '_id' not in data:
+                get_and_update_vehicle(data)
+                return
+
+            vehicle_id = data['_id']
 
             if vehicle_id not in active_client_sessions:
                 active_client_sessions[vehicle_id] = []
